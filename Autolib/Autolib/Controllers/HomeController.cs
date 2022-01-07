223dc9e041c27b1e58a1282e6564e88a8ef24032
Tim <tim.kosak@etu.univ-lyon1.fr>
2022-01-07 07:59:42 +0100

    Init

---
+++ Autolib/Autolib/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Autolib.Models;
+using Autolib.Models.Dao;
+using Autolib.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autolib.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Vehicule monVehicule = null;
+
+            try
+            {
+                monVehicule = ServiceVehicule.getInstance().GetVehicule(5);
+            }catch(Exception e)
+            {
+                ModelState.AddModelError("Erreur", "Echec de la récupération d'un vehicule" + e.Message);
+            }
+            return View(monVehicule);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Autolib/Autolib/Models/Dao/ServiceVehicule.cs
@@ -0,0 +1,41 @@
+﻿using Autolib.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autolib.Models.Dao
+{
+    public class ServiceVehicule
+    {
+        private static ServiceVehicule instance;
+        private static autolibContext context;
+
+        public static ServiceVehicule getInstance()
+        {
+            if(ServiceVehicule.instance == null)
+            {
+                ServiceVehicule.instance = new ServiceVehicule();
+                ServiceVehicule.context = new autolibContext();
+            }
+
+            return ServiceVehicule.instance;
+        }
+
+        public Vehicule GetVehicule(int id)
+        {
+            Vehicule vec = null;
+
+            try
+            {
+                vec = (from v in context.Vehicule
+                              where v.IdVehicule == id
+                              select v).FirstOrDefault();
+                return vec;
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
